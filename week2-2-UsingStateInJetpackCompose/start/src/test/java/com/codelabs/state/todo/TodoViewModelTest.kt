ef4d6bc26883f196c48ebc4c41feafca33cf6fb8
Jiyeon <wjy0218@gmail.com>
2021-11-21 20:51:19 +0900

    Update: Step 10

--- week2-2-UsingStateInJetpackCompose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ week2-2-UsingStateInJetpackCompose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,31 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
 class TodoViewModelTest {
-    // TODO: Write tests
+
+    @Test
+    fun whenRemovingItem_updatesList() {
+        // before
+        val viewModel = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        viewModel.addItem(item1)
+        viewModel.addItem(item2)
+
+        // during
+        viewModel.removeItem(item1)
+
+        // MutableState<T> write 가 다른 스레드에서 이루어질 경우 결과가 늦게 반영될 수 있음.
+        // 저수준 API 로 Snapshot.sendApplyNotifications() 를 호출해야함 (고수준 API 는 개발 진행중)
+
+        // after
+        assertThat(viewModel.todoItems).isEqualTo(listOf(item2))
+    }
+
+    // DB 비동기 처리 결과 테스트는 이를 기다리는 API 가 없어 현재 불가능 (1.0 버전 전에 준비되지 않음)
+
 }
